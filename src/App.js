65df2ddcf13bd42ac11a5da220806af57dea7663
Sigmadelta8 <72050600+Sigmadelta8@users.noreply.github.com>
2025-03-12 13:45:23 -0700

    Initial Commit
    React scaffold, tailwind set up, head adjusted

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="bg-amber-400">
+        <img src="https://media1.tenor.com/m/A-SAIQsYfoYAAAAd/si-va-in-scena-showtime.gif"></img>
+        </p>
+        <button className="bg-yellow-500">
+          This is literally to see if Tailwind is working (it is)
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
